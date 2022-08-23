5bf2b365efde1d9fd0384d2c7a2dce4d50a1bf18
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2022-08-23 14:50:33 -0300

    player + enemy1 atk done

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// HTML ELEMENTS //////////
+let startBtn = document.getElementById("startBtn")
+let playerHealthStatus = document.getElementById("playerStatus")
+let enemyHealthStatus = document.getElementById("enemyStatus")
+let gameLogTxt = document.querySelector("#gameLog p")
+let attackBtn = document.getElementById("attackBtn")
+let hpBtn = document.getElementById("hpBtn");
+// END HTML ELEMENTS //////////
+//
+// CHARACTER CLASSES //////////
+//
 class Characters {
     constructor(name, health, strength) {
         this.name = name
@@ -11,110 +22,115 @@ class Characters {
         this.health -= dmg
         if (this.health > 0) {
             return goToGameLog(`${this.name} has received ${dmg} points of damage.`)
-        } else if (health < 1) {
+        } else if (health <= 0) {
             return goToGameLog(`${this.name} is dead!`)
         }
     }   
 }
+//
+//_// Player Config
+class Player extends Characters {
+    constructor(name, health, strength) {
+        super(name, health, strength);
+        this.fullHealth = health;
+    }
+    receiveDamage(dmg) {
+        this.health -= dmg
+        console.log(this.health)
+        if (this.health > 0) {
+            return goToGameLog(`${this.name} has received ${dmg} points of damage.`)
+        } else if (this.health <= 0) {
+            return goToGameLog(`You died! Game Over!`)
+        }
+    }
+    healingPotion() { //ERROR. Não esta retornando `Your health is full.`
+        const heal = 15
+        console.log(this.fullHealth)
+        this.health += heal
+        console.log(this.health)
+        if (this.health >= this.fullHealth) {
+            this.health = this.fullHealth
+            playerHealthStatus.innerText = player.health
+            return goToGameLog(`Your health is full.`)
+        } else {
+            playerHealthStatus.innerText = player.health
+            return goToGameLog(`You healed ${heal} points of health.`)
+        }
+    }
+}
+let player = new Player("Joseph Climber", 100, 10)
+//_// END Player Config
+//
+//_// ENEMIES
+//
+//_//_// Skeleton - Enemy 1
+class Skeleton extends Characters {
+    constructor(name, health, strength) {
+        super(name, health, strength)
+    }
+    receiveDamage(dmg) {
+        this.health -= dmg
+        if (this.health > 0) {
+            return goToGameLog(`${this.name} has received ${dmg} points of damage.`)
+        } else if (this.health <= 0) {
+            goToGameLog(`${this.name} is dead!`)
+            // RESSURRECTION SKILL
+            setTimeout(() => {
+                goToGameLog(`Skeleton is alive again! Do something while he's knocked out!`) 
+                this.health = 50
+                enemyHealthStatus.innerText = skeleton.health
+            }, 6000)
+        }
+    }   
+}
+let skeleton = new Skeleton("Skeleton", 50, 18)
+//_//_// END Skeleton - Enemy 1
+//
+//_// END ENEMIES
+//
+// END CHARACTER CLASSES //////////
+//
 
-
-
-
-
+playerHealthStatus.innerText = player.health
+enemyHealthStatus.innerText = skeleton.health
 
 function start() {
     document.getElementById("start").style.display = "none";
     document.getElementById("gameDisplay").style.display = "block";
 }
 function goToGameLog(log) {
-    document.querySelector("#gameLog p").style.display = "block"
-    document.querySelector("#gameLog p").innerText = log
-    return setTimeout(() => document.querySelector("#gameLog p").style.display = "none", 3000)
+    gameLogTxt.style.display = "block"
+    gameLogTxt.innerText = log
+    return setTimeout(() => gameLogTxt.style.display = "none", 5000)
+}
+function enemyAttack() {
+    let dmg = player.receiveDamage(skeleton.attack())
+    playerHealthStatus.innerText = player.health
+    return dmg
 }
 function playerAttack() {
     let dmg = skeleton.receiveDamage(player.attack())
+    enemyHealthStatus.innerText = skeleton.health
     return dmg
 }
 
-
-
-
-
 //
-// PLAYER CONFIG //////////
-//
-class Player extends Characters {
-    constructor(name, health, strength) {
-        super(name, health, strength)
-    }
-    receiveDamage(dmg) {
-        this.health -= dmg
-        if (this.health > 0) {
-            return goToGameLog(`${playerName} has received ${dmg} points of damage.`)
-        } else if (this.health < 1) {
-            return goToGameLog(`You died! Game Over!`)
-        }
-    }
-    fullHealth = this.health
-    healingPotion() {
-        const heal = 15
-        this.health += heal
-        if (this.health >= this.fullHealth) {
-            this.health = this.fullHealth
-            return goToGameLog(`Your health is full.`)
-        } else {
-            return goToGameLog(`You healed ${heal} points of health.`)
-        }
-    }
-}
-let player = new Player("Joseph Climber", 100, 10)
-console.log(player)
-//
-// END PLAYER CONFIG ////////////
-//
-//
-
-
-
-
-
-// ENEMIES /////////////
-//
-// Skeleton - Enemy 1
-class Skeleton extends Characters {
-    constructor(name, health, strength) {
-        super(name, health, strength)
-        this.fullHealth = health
-    }
-    receiveDamage(dmg) {
-        this.health -= dmg
-        if (this.health > 0) {
-            return goToGameLog(`${this.name} has received ${dmg} points of damage.`)
-        } else if (this.health < 1) {
-            setInterval(this.ressurrect(), 4000)
-            return goToGameLog(`${this.name} is dead!`)
-        }
-    }   
-    ressurrect() {
-        this.health = this.fullHealth
-        return goToGameLog(`${this.name} is back alive again! Do something while he's knocked out!`)
-    }
-}
-let skeleton = new Skeleton("Skeleton", 50, 18)
-console.log(skeleton)
-// END Skeleton - Enemy 1
-
-
-
-
-
+// CLICK EVENTS //////////
 window.addEventListener('load', () => {
-    const startBtn = document.getElementById("startBtn");
     startBtn.addEventListener('click', start)
 
-    const attackBtn = document.getElementById('attackBtn');
-    attackBtn.addEventListener('click', playerAttack);
+    attackBtn.addEventListener('click', () => {
+        attackBtn.setAttribute("disabled", true)
+        attackBtn.innerHTML// IMG
+        playerAttack();
+        setTimeout(enemyAttack, 2000)
+        setTimeout(() => {
+            attackBtn.removeAttribute("disabled")
+            attackBtn.innerHTML //IMG
+        }, 4000)
+    });
   
-    const hpBtn = document.getElementById('hpBtn');
     hpBtn.addEventListener('click', player.healingPotion);
 });
+// END CLICK EVENTS //////////
+//
